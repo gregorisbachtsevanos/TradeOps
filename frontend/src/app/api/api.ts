9ebf70f7a24eb71b400c646b31bde2bd88709d25
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-07-10 11:19:07 +0300

    fix: infinity redirection to login page

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -13,10 +13,5 @@ export const apiService = axios.create({
 // Interceptor (global)
 apiService.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = "/";
-    }
-    return Promise.reject(error);
-  },
+  (error) => Promise.reject(error),
 );
